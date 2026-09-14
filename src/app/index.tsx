a37121f0f5bc6230c6e7a3543a423f2d52cb1cc6
ilham ghaffar afriansyah <ilham.ghaffar.afriansyah24012@mhs.uingusdur.ac.id>
2026-09-14 19:11:19 +0700

    feat: gabungkan komponen di halaman utama dan tambah useEffect

---
+++ src/app/index.tsx
@@ -0,0 +1,25 @@
+import { View } from "react-native";
+import { useState, useEffect } from "react";
+import WeatherCard from "../components/WeatherCard";
+import SearchBox from "../components/SearchBox";
+import RiwayatList from "../components/RiwayatList";
+export default function HalamanUtama() {
+  const [kotaAktif, setKotaAktif] = useState("Pekalongan");
+  const [riwayat, setRiwayat] = useState<string[]>(["Pekalongan"]);
+  useEffect(() => {
+    console.log("Kota aktif berubah menjadi:", kotaAktif);
+  }, [kotaAktif]);
+  function handleCari(kota: string) {
+    setKotaAktif(kota);
+    if (!riwayat.includes(kota)) {
+      setRiwayat([...riwayat, kota]);
+    }
+  }
+  return (
+    <View style={{ padding: 16, gap: 16 }}>
+      <SearchBox onCari={handleCari} />
+      <WeatherCard kota={kotaAktif} suhu={29} tingkatAQI="BAIK" />
+      <RiwayatList daftarKota={riwayat} />
+    </View>
+  );
+}
